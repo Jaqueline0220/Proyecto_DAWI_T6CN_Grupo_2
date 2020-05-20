f4cf1236713c89a034eaf1f2423e59456908243b
joedca86 <joedca86@gmail.com>
2020-05-19 22:42:21 -0500

    Primer commit como DB

--- src/main/java/com/proyecto/ProyectoDawiiiApplication.java
+++ src/main/java/com/proyecto/ProyectoDawiiiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoDawiiiApplication {
 
 	public static void main(String[] args) {
+		//db
 		SpringApplication.run(ProyectoDawiiiApplication.class, args);
 	}
 
